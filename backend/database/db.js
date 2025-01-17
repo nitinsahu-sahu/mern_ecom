797d78dbd25cc1320b0baeeecf577c7d81709887
Rajkumar Choudhary <rajkumar.c@gbhin.com>
2025-01-17 12:45:30 +0530

    Update homepage, login,signup,product card

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,15 @@
-require('dotenv').config()
-const mongoose=require("mongoose")
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-exports.connectToDB=async()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('connected to DB');
-    } catch (error) {
-        console.log(error);
-    }
-}
+exports.connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("connected to DB");
+  } catch (error) {
+    console.log("Error connecting to DB:", error);
+    process.exit(1); // Exit the process with failure
+  }
+};
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,27 +1,28 @@
-import React, { useEffect } from 'react'
-import { Navbar } from '../features/navigation/components/Navbar'
-import { ProductList } from '../features/products/components/ProductList'
-import { resetAddressStatus, selectAddressStatus } from '../features/address/AddressSlice'
-import { useDispatch, useSelector } from 'react-redux'
-import {Footer} from '../features/footer/Footer'
+import React, { useEffect } from "react";
+import { Navbar } from "../features/navigation/components/Navbar";
+import { ProductList } from "../features/products/components/ProductList";
+import {
+  resetAddressStatus,
+  selectAddressStatus,
+} from "../features/address/AddressSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { Footer } from "../features/footer/Footer";
 
 export const HomePage = () => {
+  const dispatch = useDispatch();
+  const addressStatus = useSelector(selectAddressStatus);
 
-  const dispatch=useDispatch()
-  const addressStatus=useSelector(selectAddressStatus)
-
-  useEffect(()=>{
-    if(addressStatus==='fulfilled'){
-
-      dispatch(resetAddressStatus())
+  useEffect(() => {
+    if (addressStatus === "fulfilled") {
+      dispatch(resetAddressStatus());
     }
-  },[addressStatus])
+  }, [addressStatus]);
 
   return (
     <>
-    <Navbar isProductList={true}/>
-    <ProductList/>
-    <Footer/>
+      <Navbar isProductList={true} />
+      <ProductList />
+      <Footer />
     </>
-  )
-}
+  );
+};
